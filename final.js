0e0f1d7cd92fdeb4b4e8a65169cbcfe9843bbb99
Hardikk <kambojpushkal@gmail.com>
2023-07-09 20:35:42 +0530

    added server in single file

--- final.js
+++ final.js
@@ -3,8 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const path = require("path");
 const {  Configuration, OpenAIApi } = require("openai");
+require("dotenv").config()
 const configuration = new Configuration({
-    apiKey: `Enter your API`,
+    apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
@@ -15,6 +16,14 @@ app.use(bodyParser.json());
 // Serve the index.html file
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/quiz", (req, res) => {
+  res.sendFile(path.join(__dirname, "quiz.html"));
+});
+
+app.get("/explain", (req, res) => {
+  res.sendFile(path.join(__dirname, "Explainer.html"));
 });
 
 // POST endpoint handler
@@ -50,6 +59,56 @@ if (summary.includes('%$') ){
       expertsummary: expertsummary
     });
   }
+  
+});
+app.post("/quiz-text", async(req, res) => {
+  const text = req.body.text;
+
+//   // Perform your desired operation on the received text
+const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo-16k",
+    messages: [{role: "user", 
+    content: ` for the given content do the following : 
+    Act as a quiz master who will be generating a 5 question mcq quiz based on the content provided, 
+    Make the quiz such that each qustion is more difficult than the last.
+    Add radio for each question an insure to add a "?" after each question.
+    Start the output directly with the question nothing before that.
+    ${text}`
+  }],
+});
+
+const quiz = completion.data.choices[0].message?.content;
+  
+    // Send the processed text back to the client
+    res.json({ 
+      quiz:  quiz,
+    });
+  
+  
+});
+
+app.post("/explain-text", async(req, res) => {
+  const text = req.body.text;
+
+//   // Perform your desired operation on the received text
+const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo-16k",
+    messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", 
+    content: `for the given content give an explaination of what it is and real life examples 
+    to solidify the understanding. Explain in simple words in max 100 words.
+    ${text}`
+  }],
+});
+
+const explaination = completion.data.choices[0].message?.content;
+
+
+  
+    // Send the processed text back to the client
+    res.json({ 
+      explaination: explaination
+    });
+  
   
 });
 
